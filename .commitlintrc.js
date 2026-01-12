7ea5dcedd2f9fa3bd11456f3d5d81cd0aaa2c6ec
Chukwuka Akibor <chukwuka.akibor@toptal.com>
2026-01-12 17:11:55 -0600

    fix(lint): increase body-max-line-length to 120 chars
    PR descriptions need more space for tables and
    formatted content.

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', 72],
-    'body-max-line-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 120],
     'type-enum': [
       2,
       'always',
